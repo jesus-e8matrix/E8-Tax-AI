a7b443ef80338e2d4fd42a77bc14c04287c896ce
jesus-e8matrix <jesus@e8matrix.com>
2025-10-30 16:05:52 -0400

    Update AppShell.tsx

--- src/app/components/layout/AppShell.tsx
+++ src/app/components/layout/AppShell.tsx
@@ -5,9 +5,9 @@ import Sidebar from './Sidebar';
 
 export default function AppShell({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen w-full bg-neutral-50">
+    <div className="min-h-screen w-full bg-neutral-50 md:pl-64">
       <Sidebar />
-      <div className="flex-1">
+      <div className="flex min-h-screen flex-col">
         <Header />
         <main className="mx-auto max-w-4xl px-4 sm:px-6 py-6">{children}</main>
       </div>
